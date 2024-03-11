4c544e02199eaa349a5b25149146a43ae35edf7a
Jonas Flückiger <fluckiger.jonas.dev@gmail.com>
2024-03-11 22:17:37 +0100

    Added fake data

--- frontend/src/views/TournamentsView.vue
+++ frontend/src/views/TournamentsView.vue
@@ -1,7 +1,40 @@
-<script>
+<script setup>
+// Dummy data
+let data = {
+    tournaments: [
+        {
+            title: "CS2 - 5v5",
+            desc: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
+            state: "OPEN",
+            imageUrl: "https://i.ytimg.com/vi/aRRQuvD4E5E/maxresdefault.jpg",
+            maxPlayerPerTeam: 3,
+            gameID: 1,
+
+        },
+        {
+            title: "CS2 - 2v2",
+            desc: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
+            state: "OPEN",
+            imageUrl: "https://i.ytimg.com/vi/aRRQuvD4E5E/maxresdefault.jpg",
+            maxPlayerPerTeam: 3,
+            gameID: 1,
+
+        },
+        {
+            title: "CS2 - 1v1",
+            desc: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
+            state: "OPEN",
+            imageUrl: "https://i.ytimg.com/vi/aRRQuvD4E5E/maxresdefault.jpg",
+            maxPlayerPerTeam: 3,
+            gameID: 1,
+        },
+    ]
+};
 </script>
 
 <template>
     <h1>Tournois</h1>
     <h2>Helloooo</h2>
+    <p>{{ data["tournaments"][0]["title"] }}</p>
+    <img :src="data['tournaments'][0]['imageUrl']">
 </template>
