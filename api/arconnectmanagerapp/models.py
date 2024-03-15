2991d498476bcf68e3f16ea88e59a574ca96c854
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-03-15 11:09:47 +0100

    Add tournament model

--- api/arconnectmanagerapp/models.py
+++ api/arconnectmanagerapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TournamentItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    state = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
