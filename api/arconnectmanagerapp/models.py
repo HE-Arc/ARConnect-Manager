a2385b68f4701d115e4a6d0c26b9f9962a265e63
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-03-15 21:40:37 +0100

    Add comments to tournamentItem model

--- api/arconnectmanagerapp/models.py
+++ api/arconnectmanagerapp/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 #Tournament model
 class TournamentItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    state = models.IntegerField()
+    name = models.CharField(max_length=100)             #Name of the tournament
+    description = models.TextField()                    #Description of the tournament
+    state = models.IntegerField()                       #State of the tournament : 0=open, 1=started, 2=finished
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
