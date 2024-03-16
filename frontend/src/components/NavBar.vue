1c9d6d527c42de0914e2b1cf4737440ee9027ce9
Jonas Flückiger <fluckiger.jonas.dev@gmail.com>
2024-03-16 23:00:54 +0100

    DONE basic tournaments page
    Added a simple page where all tournaments are displayed. Also created an empty page for tournament details.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -19,16 +19,17 @@
 
 nav {
   display: flex;
-  padding: 14px 64px 14px 64px;
+  padding: 1em 3% 1em 3%;
   justify-content: space-between;
   align-items: center;
 }
 
 div#navbar-links {
   display: flex;
-  gap: 64px;
-  justify-content: space-around;
+  gap: 5%;
+  justify-content: end;
   align-items: center;
+  flex: 1;
 }
 
 a {
---
+++ frontend/src/components/TournamentPreview.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const props = defineProps(['tournament'])
+const tournament = props.tournament;
+
+function navigateToTournamentDetails() {
+    router.push({ name: "tournamentDetails", params: { tournamentId: tournament.id } })
+}
+</script>
+
+
+<template class="test">
+    <div class="tournament-container" @click="navigateToTournamentDetails">
+        <p>{{ tournament.name }}</p>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+.tournament-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1em;
+    background-image: url("@/assets/images/arconnect_background.png");
+    text-align: center;
+    transition: all 0.25s ease-out;
+
+    &:hover {
+        scale: 110%;
+        cursor: pointer;
+        box-shadow: rgba($color: #000000, $alpha: 0.2) 0px 0px 10px 10px;
+    }
+
+    p {
+        margin: 16px;
+    }
+
+}
+</style>
