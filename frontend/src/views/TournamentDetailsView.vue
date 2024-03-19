e5733fca1e3f62467e2aa7bcf72a3cc5efb91d34
Jonas Flückiger <fluckiger.jonas.dev@gmail.com>
2024-03-19 15:58:50 +0100

    DONE TournamentDetails

--- frontend/src/views/TournamentDetailsView.vue
+++ frontend/src/views/TournamentDetailsView.vue
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { useRoute } from 'vue-router';
 import { ref, onMounted } from 'vue';
+import { stateToString } from '@/domain/TournamentStatus'
 
 const route = useRoute();
 const tournamentId = route.params.tournamentId;
@@ -22,7 +23,15 @@ onMounted(() => {
 
 
 <template>
-    <div v-if="tournament">
+    <div v-if="tournament" class="grid-container">
         <h1>{{ tournament.name }}</h1>
+        <p>{{ tournament.description }}</p>
+        <p>{{ stateToString(tournament.state) }}</p>
     </div>
 </template>
+
+<style scoped>
+.grid-container * {
+    grid-column: 1 / span 12;
+}
+</style>
