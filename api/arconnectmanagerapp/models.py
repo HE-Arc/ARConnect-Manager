79a1a2f292b4c101eb94f83cdfd5a1f896ccf166
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-03-21 21:47:44 +0100

    Add validator to the state field

--- api/arconnectmanagerapp/models.py
+++ api/arconnectmanagerapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 #Tournament model
 class TournamentItem(models.Model):
     name = models.CharField(max_length=100)             #Name of the tournament
     description = models.TextField()                    #Description of the tournament
-    state = models.IntegerField()                       #State of the tournament : 0=open, 1=started, 2=finished
+    state = models.IntegerField(                        #State of the tournament : 0=open, 1=started, 2=finished
+            default=0,
+            validators=[MinValueValidator(0), MaxValueValidator(3)]
+        )                       
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
