083a56332dc06255127f7618bd413ec5e50e09cd
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2024-03-21 09:53:03 +0100

    WIP Deployment

--- api/arconnectmanager/settings.py
+++ api/arconnectmanager/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env('ALLOWED_HOST')]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,8 @@ set :repo_url, "https://github.com/HE-Arc/ARConnect-Manager.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Créer un lien symbolique vers le fichier .env dans le répertoire current/backend
 after 'deploy:symlink:release', 'deploy:create_env_symlink'
 namespace :deploy do
     desc 'Create symlink for .env file'
@@ -46,3 +48,39 @@ namespace :deploy do
       end
     end
   end
+  
+# Installer les dépendances Python
+after 'deploy:updating', 'pip:install'
+namespace :pip do
+    desc 'Install'
+    task :install do
+        on roles([:app, :web]) do |h|
+        execute "pip install -r #{release_path}/api/requirements.txt"
+        end
+    end
+end
+
+# Construire et déployer l'application Vue.js
+after 'deploy:updated', 'vue:deploy'
+namespace :vue do
+  desc 'Build and deploy Vue.js application'
+  task :deploy do
+    on roles(:app) do
+      within release_path.join('frontend') do
+        execute :npm, 'install' # Installer les dépendances npm
+        execute :npm, 'run build' # Construire l'application Vue.js
+      end
+    end
+  end
+end
+
+# Redémarrer le serveur Gunicorn
+after 'deploy:publishing', 'gunicorn:restart'
+namespace :gunicorn do
+    desc 'Restart application'
+    task :restart do
+        on roles(:web) do |h|
+        execute :sudo, 'systemctl restart gunicorn'
+    end
+    end
+end
