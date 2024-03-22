d3ad9e6cda497b4cd889a35b183d591528bcea3d
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2024-03-22 12:50:40 +0100

    Add the use of postgresSQL to prod

--- api/arconnectmanager/settings.py
+++ api/arconnectmanager/settings.py
@@ -104,13 +104,26 @@ WSGI_APPLICATION = 'arconnectmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Configuration de la base de données pour l'environnement local
+if env('ENVIRONMENT') == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+# Configuration de la base de données pour l'environnement de production
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('POSTGRES_DATABASE'),
+            'USER': env('POSTGRES_USER'),
+            'PASSWORD': env('POSTGRES_PASSWORD'),
+            'HOST': env('POSTGRES_HOST'),
+            'PORT': env('POSTGRES_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
