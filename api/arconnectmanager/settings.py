80ec912e0b2d9fdbc487fb90c710a387502d9dc2
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2024-03-22 08:57:52 +0100

    Add address to cors origin

--- api/arconnectmanager/settings.py
+++ api/arconnectmanager/settings.py
@@ -53,6 +53,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://arconnect.k8s.ing.he-arc.ch",
 ]
 
 INSTALLED_APPS = [
