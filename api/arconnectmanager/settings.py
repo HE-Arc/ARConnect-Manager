007ff3d61d590db91faeb29c7310bbcfb0bc2b9f
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-04-28 18:40:25 +0200

    Remove old imports

--- api/arconnectmanager/settings.py
+++ api/arconnectmanager/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 
-import environ
-import os
-
 
 from pathlib import Path
 import environ
@@ -35,8 +32,6 @@ env = environ.Env()
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
