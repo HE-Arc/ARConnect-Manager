5664fe614373697ed12b6e1973419f0955cb01e9
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-04-28 18:41:55 +0200

    Update permissions for the new routes added

--- api/arconnectmanagerapp/permissions.py
+++ api/arconnectmanagerapp/permissions.py
@@ -2,13 +2,12 @@ from rest_framework import permissions
 
 class TournamentPermission(permissions.BasePermission):
     '''Permissions for the TournamentItem model'''
-    
     def has_permission(self, request, view):
         if view.action in ['list', 'retrieve']:
             return True
         elif view.action in ['register', 'unregister']:
             return request.user.is_authenticated
-        elif view.action in ['create', 'update', 'partial_update', 'destroy']:
+        elif view.action in ['create', 'update', 'partial_update', 'destroy', 'open_registration', 'api_start', 'api_finish', 'api_get_matches', 'api_submit_scores']:
             return request.user.is_authenticated and request.user.is_staff
         else:
             return False
