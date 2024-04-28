5753d02fcc5cb038558565c164942095bbe089de
AlexandreMaquet-HeArc <alexandre.maquet+1@he-arc.ch>
2024-04-28 18:41:34 +0200

    Update TournamentItem model to add fields for challongeAPI

--- api/arconnectmanagerapp/models.py
+++ api/arconnectmanagerapp/models.py
@@ -4,13 +4,16 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 #Tournament model
 class TournamentItem(models.Model):
-    name = models.CharField(max_length=100)             #Name of the tournament
-    description = models.TextField()                    #Description of the tournament
-    state = models.IntegerField(                        #State of the tournament : 0=closed, 1=open, 2=started 3=finished
+    name = models.CharField(max_length=100)                         #Name of the tournament
+    description = models.TextField()                                #Description of the tournament
+    state = models.IntegerField(                                    #State of the tournament : 0=closed, 1=open, 2=started 3=finished
             default=0,
             validators=[MinValueValidator(0), MaxValueValidator(3)]
         )                       
     players = models.ManyToManyField(User, related_name='players')
+    
+    challonge_id = models.CharField(max_length=100, blank=True)     #ID of the tournament on Challonge
+    challonge_image_url = models.URLField(blank=True)                         #URL of the image of the tournament
     
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
